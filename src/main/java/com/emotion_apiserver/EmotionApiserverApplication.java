e7e9f09fd036c78e27e1d33a0766cb0ed974001c
DeepSeeDev <hyuk2000s@naver.com>
2025-04-02 14:30:28 +0900

    first commit

---
+++ src/main/java/com/emotion_apiserver/EmotionApiserverApplication.java
@@ -0,0 +1,13 @@
+package com.emotion_apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionApiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionApiserverApplication.class, args);
+    }
+
+}
