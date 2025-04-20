5c5322b0272052541981c198f3d2bd70c4fe8a2a
DeepSeeDev <hyuk2000s@naver.com>
2025-04-20 12:07:39 +0900

    UTC응답 KST로 수정

--- src/main/java/com/emotion_apiserver/config/JacksonConfig.java
+++ src/main/java/com/emotion_apiserver/config/JacksonConfig.java
@@ -1,11 +1,18 @@
 package com.emotion_apiserver.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
 @Configuration
@@ -14,15 +21,21 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
 
-        // LocalDateTime 파싱 가능하도록 모듈 등록
-        mapper.registerModule(new JavaTimeModule());
+        // Custom Serializer 등록
+        javaTimeModule.addSerializer(LocalDateTime.class,
+                new JsonSerializer<LocalDateTime>() {
+                    @Override
+                    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers)
+                            throws IOException {
+                        gen.writeString(value.atZone(ZoneId.of("Asia/Seoul"))
+                                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                    }
+                });
 
-
+        mapper.registerModule(javaTimeModule);
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-
-        mapper.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
 
         return mapper;
     }
