d4ccf99f228b6811e85fc870622c45dec4d3958c
DeepSeeDev <hyuk2000s@naver.com>
2025-04-21 14:47:26 +0900

    JacksonConfig.java 삭제

--- src/main/java/com/emotion_apiserver/controller/emotion/AnalysisController.java
+++ src/main/java/com/emotion_apiserver/controller/emotion/AnalysisController.java
@@ -4,6 +4,7 @@ import com.emotion_apiserver.domain.dto.account.AccountDto;
 import com.emotion_apiserver.domain.dto.analysis.WeeklyEmotionAnalysisResponse;
 import com.emotion_apiserver.service.AnalysisService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class AnalysisController {
 
     private final AnalysisService analysisService;
@@ -25,6 +27,7 @@ public class AnalysisController {
 
     @GetMapping("/api/analysis/monthly")
     public ResponseEntity<WeeklyEmotionAnalysisResponse> getMonthlyAnalysis(@AuthenticationPrincipal AccountDto account) {
+        log.info("🧪 accountDto: {}", account);
         return ResponseEntity.ok(analysisService.getMonthlyAnalysis(account.getId()));
     }
 
